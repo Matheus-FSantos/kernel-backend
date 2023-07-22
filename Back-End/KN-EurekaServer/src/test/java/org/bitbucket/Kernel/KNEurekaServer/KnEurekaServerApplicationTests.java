377a758c511e0ddbe1f0227df965851ec2784211
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2023-07-22 15:14:32 -0300

    feat(back-end architeture): eureka-server (discovery server) alredy configured (PR #02)

---
+++ Back-End/KN-EurekaServer/src/test/java/org/bitbucket/Kernel/KNEurekaServer/KnEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bitbucket.Kernel.KNEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
