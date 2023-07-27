7fcb4b6096d0669a7bc1d2fe849d685088c51271
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2023-07-27 20:40:38 -0300

    KN-2/cloud gateway configuration (PR #3)
    * chore: technical documentation of the project added
    
    &
    
    * KN-2 feat: cloud gateway creation and configuration

---
+++ Back-End/KN-CloudGateway/src/test/java/io/github/MatheusFSantos/Kernel/KNCloudGateway/KnCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.MatheusFSantos.Kernel.KNCloudGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
