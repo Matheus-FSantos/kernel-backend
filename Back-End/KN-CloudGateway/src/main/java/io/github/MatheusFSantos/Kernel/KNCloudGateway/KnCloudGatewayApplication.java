085761cc0e434f8b0257b3c19fb7465c4a4b07df
matheus <matheus.fs.contato@gmail.com>
2023-08-01 13:14:24 -0300

    KN-3 refactor(gateway):  user load balancer configuration on gateway service

--- Back-End/KN-CloudGateway/src/main/java/io/github/MatheusFSantos/Kernel/KNCloudGateway/KnCloudGatewayApplication.java
+++ Back-End/KN-CloudGateway/src/main/java/io/github/MatheusFSantos/Kernel/KNCloudGateway/KnCloudGatewayApplication.java
@@ -2,7 +2,10 @@ package io.github.MatheusFSantos.Kernel.KNCloudGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -10,6 +13,14 @@ public class KnCloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KnCloudGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator router(RouteLocatorBuilder routeLocatorBuilder) {
+		return
+				routeLocatorBuilder
+					.routes().route(r -> r.path("/api/users/**").uri("lb://kn-users"))
+				.build();
 	}
 
 }
