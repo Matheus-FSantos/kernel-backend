70ed93834d4fd56e78c476fa8859847c490dc058
matheus <matheus.fs.contato@gmail.com>
2023-08-06 14:04:34 -0300

    KN-4 feat: add user repository

---
+++ Back-End/KN-Users/src/main/java/io/github/MatheusFSantos/Kernel/KNUsers/model/repository/UsersRepository.java
@@ -0,0 +1,11 @@
+package io.github.MatheusFSantos.Kernel.KNUsers.model.repository;
+
+import io.github.MatheusFSantos.Kernel.KNUsers.model.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, UUID> {
+}
