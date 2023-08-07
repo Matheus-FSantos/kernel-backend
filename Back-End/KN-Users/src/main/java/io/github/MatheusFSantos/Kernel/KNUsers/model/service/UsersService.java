5ce21baa3409093849050b760bf037147e58c003
MATHEUS FERREIRA SANTOS <matheus.fsantos52@senacsp.edu.br>
2023-08-07 10:46:14 -0300

    KN-4 refactor: refactor in save method

--- Back-End/KN-Users/src/main/java/io/github/MatheusFSantos/Kernel/KNUsers/model/service/UsersService.java
+++ Back-End/KN-Users/src/main/java/io/github/MatheusFSantos/Kernel/KNUsers/model/service/UsersService.java
@@ -1,9 +1,13 @@
 package io.github.MatheusFSantos.Kernel.KNUsers.model.service;
 
+import io.github.MatheusFSantos.Kernel.KNUsers.model.entity.DTO.UsersDTO;
 import io.github.MatheusFSantos.Kernel.KNUsers.model.entity.Users;
+import io.github.MatheusFSantos.Kernel.KNUsers.model.enumeration.Roles;
 import io.github.MatheusFSantos.Kernel.KNUsers.model.exception.UsersException;
+import io.github.MatheusFSantos.Kernel.KNUsers.model.exception.specific.UserAlreadyExists;
 import io.github.MatheusFSantos.Kernel.KNUsers.model.exception.specific.UsersNotFound;
 import io.github.MatheusFSantos.Kernel.KNUsers.model.repository.UsersRepository;
+import io.github.MatheusFSantos.Kernel.KNUsers.model.validation.UsersValidations;
 import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,8 +34,14 @@ public class UsersService {
         return this.usersRepository.findById(id).orElseThrow(() -> new UsersNotFound("User not found in database!", "The method was invoked to list a user (method find by id), but the user is not present in the database of this service", HttpStatus.SC_NOT_FOUND));
     }
 
-    public void save() {
+    public void save(UsersDTO usersDTO) throws UsersException {
+        UsersValidations.validation(usersDTO);
 
+        if(this.usersRepository.findByNickname(usersDTO.getNickname()).isPresent() || this.usersRepository.findByEmail(usersDTO.getEmail()).isPresent())
+            throw new UserAlreadyExists("User already exists!", "The method was invoked to save one user (save method), however this user already exists.", HttpStatus.SC_CONFLICT);
+
+        Users user = new Users(null, usersDTO.getName(), usersDTO.getNickname(), usersDTO.getPassword(), usersDTO.getLocation(), usersDTO.getLocation(), usersDTO.getBiography(), Roles.BASIC);
+        this.usersRepository.save(user);
     }
 
     public void update() {
