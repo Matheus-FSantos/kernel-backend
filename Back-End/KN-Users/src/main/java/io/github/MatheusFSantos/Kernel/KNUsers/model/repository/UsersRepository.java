0cc0516b624f892bf1695d1ca08d93dab6f3e578
MATHEUS FERREIRA SANTOS <matheus.fsantos52@senacsp.edu.br>
2023-08-07 10:45:16 -0300

    KN-4 feat: add 'findByNickname' and 'findByEmail' GET methods

--- Back-End/KN-Users/src/main/java/io/github/MatheusFSantos/Kernel/KNUsers/model/repository/UsersRepository.java
+++ Back-End/KN-Users/src/main/java/io/github/MatheusFSantos/Kernel/KNUsers/model/repository/UsersRepository.java
@@ -4,8 +4,14 @@ import io.github.MatheusFSantos.Kernel.KNUsers.model.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, UUID> {
+
+    Optional<Users> findByNickname(String nickname);
+
+    Optional<Users> findByEmail(String email);
+
 }
