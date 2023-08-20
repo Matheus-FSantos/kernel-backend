fb5b270001e5e9d9af42b7a4eac2a455cd5dd64d
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2023-08-20 18:40:33 -0300

    KN-6 refactor: refactor the UserUUIDNotValid class name to UUIDNotValid

--- Back-End/KN-Users/src/main/java/io/github/MatheusFSantos/Kernel/KNUsers/model/exception/specific/UUIDNotValid.java
+++ Back-End/KN-Users/src/main/java/io/github/MatheusFSantos/Kernel/KNUsers/model/exception/specific/UUIDNotValid.java
@@ -5,21 +5,21 @@ import io.github.MatheusFSantos.Kernel.KNUsers.model.exception.UsersException;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class UserUUIDNotValid extends UsersException {
+public class UUIDNotValid extends UsersException {
 
-    public UserUUIDNotValid(List<String> messages, String description, Integer code) {
+    public UUIDNotValid(List<String> messages, String description, Integer code) {
         super(messages, description, code, LocalDateTime.now());
     }
 
-    public UserUUIDNotValid(String messages, String description, Integer code) {
+    public UUIDNotValid(String messages, String description, Integer code) {
         super(messages, description, code, LocalDateTime.now());
     }
 
-    public UserUUIDNotValid(List<String> messages, Integer code) {
+    public UUIDNotValid(List<String> messages, Integer code) {
         super(messages, "No description available.", code, LocalDateTime.now());
     }
 
-    public UserUUIDNotValid(String messages, Integer code) {
+    public UUIDNotValid(String messages, Integer code) {
         super(messages, "No description available.", code, LocalDateTime.now());
     }
 
--- Back-End/KN-Users/src/main/java/io/github/MatheusFSantos/Kernel/KNUsers/model/util/validation/UUIDValidation.java
+++ Back-End/KN-Users/src/main/java/io/github/MatheusFSantos/Kernel/KNUsers/model/util/validation/UUIDValidation.java
@@ -1,7 +1,7 @@
 package io.github.MatheusFSantos.Kernel.KNUsers.model.util.validation;
 
 import io.github.MatheusFSantos.Kernel.KNUsers.model.exception.UsersException;
-import io.github.MatheusFSantos.Kernel.KNUsers.model.exception.specific.UserUUIDNotValid;
+import io.github.MatheusFSantos.Kernel.KNUsers.model.exception.specific.UUIDNotValid;
 import org.apache.http.HttpStatus;
 
 public class UUIDValidation {
@@ -9,7 +9,7 @@ public class UUIDValidation {
     public static void isValidUUID(String id) throws UsersException {
         String regex = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
         if(!id.matches(regex))
-            throw new UserUUIDNotValid("Identifier invalid", "The entered identifier does not follow the standards required by the system", HttpStatus.SC_BAD_REQUEST);
+            throw new UUIDNotValid("Identifier invalid", "The entered identifier does not follow the standards required by the system", HttpStatus.SC_BAD_REQUEST);
     }
 
 }
