a16840a02c8f29f77b0adfa94c6c6d4a26c330ea
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2023-08-20 17:28:32 -0300

    KN-6 feat: add Cryptography annotation to more code semanthic

---
+++ Back-End/KN-Users/src/main/java/io/github/MatheusFSantos/Kernel/KNUsers/model/annotation/Cryptography.java
@@ -0,0 +1,13 @@
+package io.github.MatheusFSantos.Kernel.KNUsers.model.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface Cryptography {
+    boolean encodeMethod() default false;
+    boolean decodeMethod() default false;
+}
