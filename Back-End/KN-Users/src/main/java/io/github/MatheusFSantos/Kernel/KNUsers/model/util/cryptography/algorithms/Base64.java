e4c816a918a67f9f01f26255b3066a57630a371a
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2023-08-20 17:30:24 -0300

    KN-6 feat: add Base64 crypto class

---
+++ Back-End/KN-Users/src/main/java/io/github/MatheusFSantos/Kernel/KNUsers/model/util/cryptography/algorithms/Base64.java
@@ -0,0 +1,23 @@
+package io.github.MatheusFSantos.Kernel.KNUsers.model.util.cryptography.algorithms;
+
+import io.github.MatheusFSantos.Kernel.KNUsers.model.annotation.BusinessCritical;
+import io.github.MatheusFSantos.Kernel.KNUsers.model.annotation.Cryptography;
+import io.github.MatheusFSantos.Kernel.KNUsers.model.annotation.InternalUseOnly;
+
+import static java.util.Base64.*;
+
+@InternalUseOnly
+@BusinessCritical
+public class Base64 {
+
+    @Cryptography(encodeMethod=true)
+    public static String encoder(String JSON) {
+        return new String(getEncoder().encode(JSON.getBytes()));
+    }
+
+    @Cryptography(decodeMethod=true)
+    public static String decode(String encryptedString) {
+        return new String(getDecoder().decode(encryptedString.getBytes()));
+    }
+
+}
